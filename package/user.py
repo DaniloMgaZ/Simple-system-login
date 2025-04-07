9f068cb44ac67f464521e28d34fd836c9332652c
Danilo Fernandes <ceusouop@gmail.com>
2025-04-07 20:18:24 -0300

    Update user.py
    Basic change

--- package/user.py
+++ package/user.py
@@ -12,31 +12,3 @@ class Login:
         # passwordPRINT = account.get("password")
         Login.lista.append(account)
         return account #userPRINT, passwordPRINT
-
-# c = ''
-# while c != 'N':
-#     user = input("User: ")
-#     password = input("Password: ")
-
-#     pessoa = Login(user, password)
-#     c = input(": ")
-
-
-# user = input("User: ")
-# password = input("Password: ")
-
-# pessoa = Login(user, password)
-
-# print(pessoa.createAccount())
-# userPRINT, passwordPRINT = pessoa.createAccount()
-
-# print("\n","Usuário:", userPRINT)
-# print(" Senha:", passwordPRINT)
-
-# account = pessoa.createAccount()
-
-# user = account.get("user")
-# password = account.get("password")
-
-# print(f"Usuario: {user}")
-# print(f"Senha: {password}")
